5c1f515d2e8c42966b896d2531d5349cd480e5e6
Vannnd <you@example.com>
2024-09-19 21:57:10 +0700

    test button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("ini adalah jawascript dari ytta file")
+
+// membuat fungsi 
+
+function klik() {
+    console.log('test')
+    document.getElementById('kmplk').innerHTML="gimana hari ini, apa kamu baik baik saja? <br> bagaimana kabar kamu?" 
+}
